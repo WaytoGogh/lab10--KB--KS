5a7e529e794dd0003eb53b4f9f5d67fde2c0d2a0
WaytoGogh <kcreatorgrace@gmail.com>
2025-04-09 11:25:12 -0400

    calculator.py

--- calculator.py
+++ calculator.py
@@ -1,3 +1,6 @@
+#Edits done by Kezia Saint-Hilaire
+import math
+
 """
 calculator.py
 - Defines functions used to create a simple calculator
@@ -6,6 +9,31 @@ One function per operation, in order.
 """
 # First example
 def add(a, b): 
-    pass
+    return a+b
+
+#Function to subtract
+def sub(a, b):
+    return a-b 
+
+#function to multiply
+def mul(a,b):
+    return a*b
+
+#function to divide, with error handling for division with zero
+def div(a,b):
+    if a==0:
+        raise ZeroDivisionError("Cannot divide by zero.")
+    return b/a
+
+# function to calculate logarithm of b to the base of a with error handling
+def log(a, b):
+    if a<= 0 or a == 1:
+        raise ValueError("Logarithmic base must be greater than 0 and not equal to 1.")
+    if b <= 0:
+        raise ValueError("Logarithm argument must be greater than 0.")
+    return math.log(b, a)
+def exp(a, b):
+    return a ** b
+
 
 
